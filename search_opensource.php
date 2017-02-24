30d32647d455dc268bd0aa1ea2fff6f227a1ef03
Tsubasa Kato <tsubasa_kato@hotmail.com>
2017-02-24 17:12:34 +0900

    quick fix

--- search_opensource.php
+++ search_opensource.php
@@ -5,8 +5,7 @@
     mysql_connect("localhost", $username, $password) or die("Error connecting to database: ".mysql_error());
     /*
         localhost - it's location of the mysql server, usually localhost
-        root - your username
-        third is your password
+       
          
         if connection fails it will stop loading the page and display an error
     */
